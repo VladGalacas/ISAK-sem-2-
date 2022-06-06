46470ec1847eed4da377883348c493c7eac9e535
VladGalacas <95177254+VladGalacas@users.noreply.github.com>
2022-06-06 18:14:02 +0300

    Update and rename ЛР5_1.md to ЛР5_1.sh

---
+++ ЛР5_1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 1 task
+echo "input message:"
+read msg
+echo $msg
+
+echo "input date and users:"
+read users 
+arr=( $users )
+
+# 2 task
+for user in ${arr[@]:1}; do
+	if getent passwd | grep -qe $user; then 
+		echo $user is registred; 
+	else 
+		echo $user is not regitred; 
+	fi; 
+done
+
+# 3 task
+last -p ${arr[0]} | awk '{print $1}' | grep -v 'reboot' | grep -v 'wtmp' >time_users
+read date_users <time_users
+
+for user in ${arr[@]:1}; do
+	case $user in 
+		$date_users)
+			echo $msg | mail $user
+	esac;
+done
+
+
+# 4 task
+who | awk '{print $1}' >users
+read all_now_work_users <users
+
+echo $all_now_work_users | grep -v $date_users >now_users
+read now_users <now_users
+
+for u in $now_users; do
+	echo $msg | write $u
+done
